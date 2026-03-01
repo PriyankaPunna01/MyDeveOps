439c60c604ed356462b2fed14ba865d1ece326d7
PriyankaPunna01 <priyankapunna01@gmail.com>
2026-03-01 16:05:37 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
    public static void main(String[] args){
         System.out.println("Hello World!!");
         System.out.println("Sies College of Management Studies ");
+        System.out.println("Sum of Two Numbers : " + (20 + 56));
    }
 }
