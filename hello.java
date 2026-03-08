71fdddd8393a9475b670d343f02c95335b0d4e65
PriyankaPunna01 <priyankapunna01@gmail.com>
2026-03-08 15:42:25 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello{
         System.out.println("Hello World!!");
         System.out.println("Sies College of Management Studies ");
         System.out.println("Sum of Two Numbers : " + (20 + 56));
+        System.out.println("Successfully Triggered the WebHoook");
    }
 }
